7213bc22bc5f43ca3c520f6f6b575721f55e1123
ianpokorny <ianpokorny@gmail.com>
2026-05-12 19:05:52 -0400

    add s14

--- examples/S14-keyboardHID/S14-keyboardHID.ino
+++ examples/S14-keyboardHID/S14-keyboardHID.ino
@@ -3,9 +3,11 @@
 ||||||||||||||||||||||||||
 ||    "Keyboard HID"    ||
 ||||||||||||||||||||||||||
-- Turns microcontroller into a USB keyboard, a "Human-Interface-Device" or HID for short
-- button 1 = A
-- button 2 = Z
+
+- Turns microcontroller into a USB keyboard, a "Human-Interface-Device" or HID for short.
+
+- button 1 = letter
+- button 2 = message
 
 - NOTE: You MUST set USB MODE to "USB-OTG (TinyUSB)" under Tools menu
 - NOTE: you cannot use GPIO Pins 19 & 20 when using USB comms
@@ -20,23 +22,32 @@ https://docs.arduino.cc/built-in-examples/usb/KeyboardAndMouseControl/
 #include "USB.h"
 #include "USBHIDKeyboard.h"
 
-//create keyboard object
-USBHIDKeyboard Keyboard;
+//include our own Button Class (this must be copied into arduino project folder)
+#include "Button.h"
 
 //Define Pins
 const int buttonPin_1 = 1;
 const int buttonPin_2 = 2;
 
-//Button Variables
-bool buttonState_1 = 0;
-bool buttonState_2 = 0;
-bool prevButtonState_1 = 0;
-bool prevButtonState_2 = 0;
+//array of letters in alphabet
+const char alphabet[] = 
+{'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+'o', 'p', 'q', 'r', 's', 't', 'u', 
+'v', 'w', 'x', 'y', 'z'};
+
+//alphabet array index
+int letter = 0;
+
+//create keyboard object
+USBHIDKeyboard Keyboard;
+
+// Create new button objects from Button class
+Button button1(buttonPin_1, INPUT_PULLUP);
+Button button2(buttonPin_2, INPUT_PULLUP);
 
 void setup() 
 {
-  pinMode(buttonPin_1, INPUT_PULLUP); 
-  pinMode(buttonPin_2, INPUT_PULLUP); 
   Serial.begin(9600); //start serial comm @ 9600 baud rate
   Keyboard.begin(); //init keyboard object
   USB.begin();  //init USB object
@@ -44,21 +55,32 @@ void setup()
 
 void loop() 
 {
-  buttonState_1 = !digitalRead(buttonPin_1);  //read button state 1
-  buttonState_2 = !digitalRead(buttonPin_2);  //read button state 2
+  //read and update button states
+  button1.update();
+  button2.update();
 
-  if (buttonState_1 && !prevButtonState_1)  //if rising edge detected...
+  if (button1.risingEdge())  //if rising edge detected...
   {
-    Keyboard.write('A');  //send keypress of 'A'
-    //you can also use Keyboard.print('A') or Keyboard.println('A') if you want a newline afterwards
+
+    Keyboard.write(alphabet[letter]); //send keyboard write command of a single char
+    //you can also use Keyboard.print()
+
+    letter++; //increment letter
+    if (letter > 25) // if we have reached the end of the alphabet...
+    {
+      Keyboard.println(); //print a new line
+      letter = 0; //reset letter to 0
+    }
   }
-  prevButtonState_1 = !digitalRead(buttonPin_1);
   
-  if (buttonState_2 && !prevButtonState_2)  //if rising edge detected...
+  if (button2.risingEdge())  //if rising edge detected...
   {
-    Keyboard.write('Z');  //send keypress of 'Z'
+    //print long message with newlines when button 2 is pressed
+    Keyboard.println("Hello World,");
+    Keyboard.println("This is a message from your ESP32 MCU using USB HID.");
+    Keyboard.println("You can print out whole strings of text in one command!");
   }
-  prevButtonState_2 = !digitalRead(buttonPin_2);
-  
-  delay(5); //slight delay to remove button debounce noise
+
+  delay(12); //slight delay to remove button debounce noise
+  //NOTE: add a proper timer for button debounce, delay() works for now
 }
